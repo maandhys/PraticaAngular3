c052fac0f400a867cde44610d6bc949c6f692545
nayara.pimentel <n.pimentel@vallourec.com>
2020-05-19 23:53:12 -0300

    5

--- pratica3/src/app/pessoas/pessoas.component.ts
+++ pratica3/src/app/pessoas/pessoas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Pessoas } from '../models/pessoas.model';
 
 @Component({
   selector: 'app-pessoas',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PessoasComponent implements OnInit {
 
+  pessoas: Pessoas;
   constructor() { }
 
   ngOnInit() {
